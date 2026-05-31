fcb67da1adc6384a0fcf6d4b43f5e1b214332ceb
L <rabe.janaleigh@gmail.com>
2026-05-31 19:59:57 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" data-theme="dark" suppressHydrationWarning>
       <body>{children}</body>
     </html>
   );
